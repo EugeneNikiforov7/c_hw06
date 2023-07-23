b9c6715d9bf262d78a23d67dfac2d1410943d62e
EugeneNikiforov <eugene_vn@mail.ru>
2023-07-23 16:00:51 +0500

    end

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int QP(int[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) res += 1;
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write(string.Join(", ", array));
+}
+
+int[] InputNumbers(int quantity)
+{
+    int res = 0;
+    int[] inputNumbers = new int[quantity];
+    for (int i = 0; i < inputNumbers.Length; i++)
+    {
+        inputNumbers[i] = GetInfo($"Введите число {i + 1}: ");
+    }    
+    return inputNumbers;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int m = GetInfo("Сколько чисел вы хотите ввести?: ");
+int[] inputNumbers = InputNumbers(m);
+PrintArray(inputNumbers);
+int quantityPositive = QP(inputNumbers);
+Console.WriteLine($" -> {quantityPositive}");
+
---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// k1*x+b1=k2*x+b2  (k1-k2)*x=b2-b1   x=(b2-b1)/(k1-k2)
+
+double FindX(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1)/(k1 - k2);
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+Console.WriteLine("Для расчета системы уравнений y=k1*x+b1, y=k2*x+b2 введите значения: ");
+double b1 = GetInfo("Введите b1: ");
+double k1 = GetInfo("Введите k1: ");
+double b2 = GetInfo("Введите b2: ");
+double k2 = GetInfo("Введите k2: ");
+
+double x = FindX(b1, k1, b2, k2);
+
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {k1 * x + b1})");
